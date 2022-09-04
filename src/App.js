8128166962b7559bc4eb954b972dd8f75db1cb05
Pablo <63462690+pablo-maroto@users.noreply.github.com>
2022-09-04 16:02:14 -0300

    first commit to do list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import  Form  from './Components/Form';
 
-function App() {
+
+
+const  App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="App-content">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lista de Tareas
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Form />
+        
+        
+      </div>
     </div>
   );
 }
